bdd192e8fb782f2189f3ded42ac3a4123036018e
Albina <albizuewa@gmail.com>
2025-09-07 17:39:28 -0700

    Add http users client

---
+++ clients/http/gateway/users/client.py
@@ -0,0 +1,40 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from clients.http.client import HTTPClient
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Data structure to create a new user
+    """
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+    phoneNumber: str
+
+
+class UsersGatewayHttpClient(HTTPClient):
+    """
+    Client to interact with http-gateway /api/v1/users service.
+    """
+
+    def get_user_api(self, user_id: str) -> Response:
+        """
+       Gets user data by user_id.
+
+       :param user_id: id of the user.
+       :return: Response object with response data.
+       """
+        return self.get(f'/api/v1/users/{user_id}')
+
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+        Creates a new user.
+
+        :param request: Dict with a new user data
+        :return: Response object with response data.
+        """
+        return self.post('/api/v1/users', json=request)

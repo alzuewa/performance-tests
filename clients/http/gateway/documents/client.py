e8d1c433c81b8b7fea70daa28cb58cb56926e44f
Albina <albizuewa@gmail.com>
2025-09-07 22:57:01 -0700

    7.5 Получение документов по счету

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,7 +1,39 @@
+from typing import TypedDict
+
 from httpx import Response
 
 from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client
+
+
+class TariffDict(TypedDict):
+    """
+    Tariff data structure.
+    """
+    url: str
+    document: str
+
+
+class GetTariffDocResponseDict(TypedDict):
+    """
+    Get tariff document response data structure.
+    """
+    tariff: TariffDict
+
+
+class ContractDict(TypedDict):
+    """
+    Contract data structure.
+    """
+    url: str
+    document: str
+
+
+class GetContractDocResponseDict(TypedDict):
+    """
+    Get contract document response data structure.
+    """
+    tariff: ContractDict
 
 
 class DocumentsGatewayHTTPClient(HTTPClient):
@@ -27,6 +59,13 @@ class DocumentsGatewayHTTPClient(HTTPClient):
         """
         return self.get(f'/api/v1/documents/contract-document/{account_id}')
 
+    def get_tariff_document(self, account_id: str) -> GetTariffDocResponseDict:
+        response = self.get_tariff_document_api(account_id)
+        return response.json()
+
+    def get_contract_document(self, account_id: str) -> GetContractDocResponseDict:
+        response = self.get_contract_document_api(account_id)
+        return response.json()
 
 def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
     """

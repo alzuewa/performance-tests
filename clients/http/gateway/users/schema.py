400fdc8441c417f74250ce02cda60f44f545ed9d
Albina <albizuewa@gmail.com>
2025-09-09 20:21:17 -0700

    Update users api with generated data

--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
 
+from tools.fakers import fake
+
 
 class UserSchema(BaseModel):
     """
@@ -29,11 +31,11 @@ class CreateUserRequestSchema(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     # alias from `Field` will be applied to data which is incoming from some source to validate it against Pydantic model
-    email: EmailStr
-    last_name: str = Field(alias='lastName')  
-    first_name: str = Field(alias='firstName')  
-    middle_name: str = Field(alias='middleName')  
-    phone_number: str = Field(alias='phoneNumber')  
+    email: EmailStr = Field(default_factory=fake.email)
+    last_name: str = Field(alias='lastName', default_factory=fake.last_name)
+    first_name: str = Field(alias='firstName', default_factory=fake.first_name)
+    middle_name: str = Field(alias='middleName', default_factory=fake.middle_name)
+    phone_number: str = Field(alias='phoneNumber', default_factory=fake.phone_number)
 
 
 class CreateUserResponseSchema(BaseModel):

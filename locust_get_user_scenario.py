7beb5efc5d0523203f4b8c6a6266f3a469485b1b
Albina <albizuewa@gmail.com>
2025-09-12 23:28:17 -0700

    Rewrite `get-user` scenario with custom User entity

--- locust_get_user_scenario.py
+++ locust_get_user_scenario.py
@@ -1,39 +1,20 @@
-from locust import HttpUser, between, task
+from locust import User, between, task
 
-from tools.fakers import fake
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 
 
-class GetUserScenarioUser(HttpUser):
-    # Pause between requests for each virtual user (sec.)
+class GetUserScenarioUser(User):
+    host = 'localhost'
     wait_time = between(1, 3)
 
-    # A variable to store created user data
-    user_data: dict
+    users_gateway_client: UsersGatewayHTTPClient
+    create_user_response: CreateUserResponseSchema
 
     def on_start(self) -> None:
-        """
-        on_start method is called 1 time when each virtual user session starts.
-        Here we create a new user sending a POST-request to /api/v1/users.
-        """
-        request = {
-            'email': fake.email(),
-            'lastName': fake.last_name(),
-            'firstName': fake.first_name(),
-            'middleName': fake.middle_name(),
-            'phoneNumber': fake.phone_number()
-        }
-        response = self.client.post('/api/v1/users', json=request)  # we don't pass host (use Web UI Locust for it)
-
-        # Saving received data including user ID
-        self.user_data = response.json()
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
+        self.create_user_response = self.users_gateway_client.create_user()
 
     @task
     def get_user(self):
-        """
-        The main load task: getting data about user.
-        Sending a GET-request to /api/v1/users/{user_id}.
-        """
-        self.client.get(
-            f'/api/v1/users/{self.user_data['user']['id']}',
-            name='/api/v1/users/{user_id}'  # Explicitly declaring request group name
-        )
+        self.users_gateway_client.get_user(user_id=self.create_user_response.user.id)

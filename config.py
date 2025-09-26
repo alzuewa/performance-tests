0cb72a7b71504e9b83b504c310e3c2993d05ab4b
Albina <albizuewa@gmail.com>
2025-09-26 10:11:48 -0700

    Adapt scenarios configs for load-testing-hub statistics lib

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
+import locust.stats  # Locust module responsible for gathering and storing statistics
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Import nested models
 from tools.config.grpc import GRPCClientConfig
 from tools.config.http import HTTPClientConfig
 from tools.config.locust import LocustUserConfig
+
+# Which percentiles will be shown in Locust reports
+locust.stats.PERCENTILES_TO_REPORT = [0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99, 1.0]
+
+locust.stats.CSV_STATS_INTERVAL_SEC = 5
+
+locust.stats.HISTORY_STATS_INTERVAL_SEC = 5
+
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 5
+
+locust.stats.CSV_STATS_FLUSH_INTERVAL_SEC = 5
 
 
 class Settings(BaseSettings):
